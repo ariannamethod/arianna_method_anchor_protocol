47fa4ee6da627bcfc6f40ade1b754d6f98aaf987
Arianna Method <theariannamethod@gmail.com>
2025-08-06 13:55:34 +0300

    Add files via upload

---
+++ for-codex-alpine-apk-tools/tests/test6.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+# desc: test triggers in kernel package
+
+$APK add --root $ROOT --initdb -U --repository $PWD/repo1 \
+	--repository $SYSREPO linux-grsec
+
+test -L "$ROOT"/boot/vmlinuz-grsec
+
+test -L "$ROOT"/boot/initramfs-grsec
+
